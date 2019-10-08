e3aa8009c7b74726ce239ff9ec2499e093e0c034
matta.meghana@gmail.com <matta.meghana@gmail.com>
2019-10-08 13:11:24 +0530

    added 2 questions

---
+++ original-questions/6.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// create function or direct code to give output
+// prime numbers upto 10
+
+// write down the logic here
---
+++ original-questions/7.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// create function or direct code to give output
+// convert the binary to decimal number
+
+// write down the logic here
